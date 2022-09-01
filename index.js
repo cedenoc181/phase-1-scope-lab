88d623eb4268e1c600d051b7be3ab3a77f61ddc3
Christian <cedenoc181@gmail.com>
2022-08-31 22:54:05 -0400

    commit tree 007f6cd25b1b7440d9a7eb822408c82196b9be3a

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    var upperCase = customerName.toUpperCase();
+    return upperCase;
+}
+upperCaseCustomerName();
+
+
+
+function setBestCustomer() {
+     var  bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+//  console.log(setBestCustomer());
+
+function overwriteBestCustomer(){
+    var bestCustomer = "maybe bob";
+
+    return bestCustomer;
+
+}
+//  console.log(overwriteBestCustomer());
+
+// const leastFavoriteCustomer = "john";
+
+// function leastFavoriteCustomer(){
+
+// return ;
+// }
